179c935a1250dfae702392c943da4bcb10589e21
javaMole <abhinareshit@gmail.com>
2025-04-29 13:34:59 +0530

    Jpa Example Probe Object

---
+++ JPA-Example-proj01/src/main/java/com/nit/JpaExampleProj01Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaExampleProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaExampleProj01Application.class, args);
+	}
+
+}
